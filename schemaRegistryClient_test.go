f7849f07c878d0b9e084d4cfad45e1fc753995cc
Gustavo Ferreira <gustavo.ferreira@imaginecurve.com>
2021-07-08 18:30:47 +0100

    add unit tests

--- schemaRegistryClient_test.go
+++ schemaRegistryClient_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func bodyToString(in io.ReadCloser) string {
@@ -102,4 +103,79 @@ func TestSchemaRegistryClient_CreateSchemaWithArbitrarySubjectName(t *testing.T)
 	assert.Nil(t, schema.codec)
 	assert.Equal(t, schema.schema, "test2")
 	assert.Equal(t, schema.version, 1)
+}
+
+func TestSchemaRegistryClient_GetSchemaByVersionWithArbitrarySubjectWithReferences(t *testing.T) {
+	refs := []Reference{
+		{Name: "name1", Subject: "subject1", Version: 1},
+		{Name: "name2", Subject: "subject2", Version: 2},
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		responsePayload := schemaResponse{
+			Subject:    "test1",
+			Version:    1,
+			Schema:     "payload",
+			ID:         1,
+			References: refs,
+		}
+		response, _ := json.Marshal(responsePayload)
+
+		switch req.URL.String() {
+		case "/subjects/test1/versions/1":
+			// Send response to be tested
+			rw.Write(response)
+		default:
+			require.Fail(t, "unhandled request")
+		}
+
+	}))
+
+	srClient := CreateSchemaRegistryClient(server.URL)
+	srClient.CodecCreationEnabled(false)
+	schema, err := srClient.GetSchemaByVersionWithArbitrarySubject("test1", 1)
+
+	// Test response
+	assert.NoError(t, err)
+	assert.Equal(t, schema.ID(), 1)
+	assert.Nil(t, schema.codec)
+	assert.Equal(t, schema.Schema(), "payload")
+	assert.Equal(t, schema.Version(), 1)
+	assert.Equal(t, schema.References(), refs)
+	assert.Equal(t, len(schema.References()), 2)
+}
+
+func TestSchemaRegistryClient_GetSchemaByVersionWithArbitrarySubjectWithoutReferences(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		responsePayload := schemaResponse{
+			Subject:    "test1",
+			Version:    1,
+			Schema:     "payload",
+			ID:         1,
+			References: nil,
+		}
+		response, _ := json.Marshal(responsePayload)
+
+		switch req.URL.String() {
+		case "/subjects/test1/versions/1":
+			// Send response to be tested
+			rw.Write(response)
+		default:
+			require.Fail(t, "unhandled request")
+		}
+
+	}))
+
+	srClient := CreateSchemaRegistryClient(server.URL)
+	srClient.CodecCreationEnabled(false)
+	schema, err := srClient.GetSchemaByVersionWithArbitrarySubject("test1", 1)
+
+	// Test response
+	assert.NoError(t, err)
+	assert.Equal(t, schema.ID(), 1)
+	assert.Nil(t, schema.codec)
+	assert.Equal(t, schema.Schema(), "payload")
+	assert.Equal(t, schema.Version(), 1)
+	assert.Nil(t, schema.References())
+	assert.Equal(t, len(schema.References()), 0)
 }
